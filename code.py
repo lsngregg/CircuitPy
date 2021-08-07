46a6dba17b7ec5eafc1709c0b806238364ea7604
Ellison Gregg <agreg016@gmail.com>
2021-08-07 00:25:42 -0500

    did some things

--- code.py
+++ code.py
@@ -1,17 +1,38 @@
 # this is a comment
 
 import board            # Currently set to the Feather RP2040
-import digitalio        # No idea
+import digitalio        # lib for digital pins
 import time             # Seems important
+import displayio
+import terminalio
 
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
 
-## I have no idea what's going on here. Probably turning an led on and off
+"""
+Initialize the i2c components
+"""
+
+# This is for the OLED
+i2c = board.I2C()
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+
+# How do for TSL?
+
+"""
+Initialize the display
+
+    from the sample code for the display
+
+"""
+
+from adafruit_display_text import label
+import adafruit_displayio_sh1107
+
+# SH1107 is vertically oriented 64x128
+WIDTH = 128
+HEIGHT = 64
+display = adafruit_displayio_sh1107.SH1107(display_bus, width=WIDTH, height=HEIGHT)
+
+display.show()
+
 while True:
-    led.value = True
-    time.sleep(1)
-    led.value = False
-    time.sleep(0.5)
-
-# Gonna play around with the display now
+    pass
